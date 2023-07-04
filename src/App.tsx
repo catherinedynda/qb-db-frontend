f5ce948ed0ccab8a048309dc41ad4eb6e26b3a4f
catherinedynda <56405940+catherinedynda@users.noreply.github.com>
2023-07-03 19:34:31 -0600

    working dummy form w/ keyword, member, & quotee

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Typography from '@mui/material/Typography';
 import './App.css';
 import Search from './Search';
 
@@ -7,20 +8,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <Search></Search>
+        <Typography variant="h2" component="h1">IO Quote Board 
+        Database</Typography>
       </header>
+      <Search></Search>
     </div>
   );
 }
--- src/Search.tsx
+++ src/Search.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Formik, Form } from "formik";
+import Autocomplete from "@mui/material/Autocomplete";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import Input from "@mui/material/Input";
+import InputAdornment from "@mui/material/InputAdornment";
+import InputLabel from "@mui/material/InputLabel";
+import Stack from "@mui/material/Stack";
+import SearchIcon from "@mui/icons-material/Search";
+import "./Search.css";
 
 function Search() {
     interface Member {
-        name: string,
-        member_id: number
-    };
+        name: string;
+        member_id: number;
+    }
 
     interface Quotee {
-        name: string,
-        person_id: number
+        name: string;
+        person_id: number;
     }
 
     const [error, setError] = useState(null);
@@ -20,50 +31,111 @@ function Search() {
     const [query, setQuery] = useState("");
 
     useEffect(() => {
-        fetch('http://localhost:4000/members')
-            .then(res => res.json())
-            .then(res => setMembers(res));
+        fetch("http://localhost:4000/members")
+            .then((res) => res.json())
+            .then((res) => setMembers(res));
         // have parameter for like top however many
-        fetch('http://localhost:4000/quotees')
-            .then(res => res.json())
-            .then(res => setQuotees(res));
-        // setMembers([{name: 'heemo 1', member_id: 1}, {name: 'heemo 2', member_id: 3}]);
+        fetch("http://localhost:4000/quotees")
+            .then((res) => res.json())
+            .then((res) => setQuotees(res));
     }, []);
 
+    const initialValues = {
+        keyword: "",
+        member: null,
+        quotee: null,
+    };
+
+    // or have values be any?
+    const submit = (values: object) => {
+        console.log("form submitted");
+        console.log({ values });
+    };
+
     if (error) {
-        return (
-            <p>Error: {error}</p>
-        )
+        return <p>Error: {error}</p>;
     } else {
         return (
-            <div className="search-wrapper">
-                <input
-                    type="search"
-                    name="search-keyword"
-                    id="search-keyword"
-                    className="search-input"
-                    value={query}
-                    onChange={(e) => (setQuery(e.target.value))}
-                    placeholder="keyword"
-                />
-                <label htmlFor="member-select">From member: </label>
-                <select id="member-select" name="member-select" className="search-input" defaultValue="">
-                    <option value="" key="default"></option>
-                    {members.map((member: Member) => {
-                        return <option value={member.member_id} key={member.member_id}>{member.name}</option>
-                    })}
-                </select>
-                {/* no wait this should be a like type search with dropdown type thing? I think the from member should also do that */}
-                <select id="quotee-select" name="quotee-select" className="search-input" defaultValue="">
-                    <option value="" key="default"></option>
-                    {quotees.map((quotee: Quotee) => {
-                        return <option value={quotee.person_id} key={quotee.person_id}>{quotee.name}</option>
-                    })}
-                </select>
-            </div>
-        )
+            <Container className="search-wrapper">
+                <Formik initialValues={initialValues} onSubmit={submit}>
+                    {({ handleChange, values, setFieldValue }) => (
+                        <Form>
+                            <Stack direction="row">
+                                <InputLabel htmlFor="search-keyword">
+                                    Keyword
+                                </InputLabel>
+                                <Input
+                                    id="search-keyword"
+                                    name="keyword"
+                                    className="search-input"
+                                    size="small"
+                                    onChange={handleChange}
+                                    startAdornment={
+                                        <InputAdornment position="start">
+                                            <SearchIcon />
+                                        </InputAdornment>
+                                    }
+                                />
+                                <Autocomplete
+                                    disablePortal
+                                    id="member-select"
+                                    // name="member"
+                                    className="search-input"
+                                    size="small"
+                                    options={members}
+                                    getOptionLabel={(option) => option.name}
+                                    onChange={(e, value) => {
+                                        console.log(value);
+                                        setFieldValue(
+                                            "member",
+                                            value !== null
+                                                ? value
+                                                : initialValues.member
+                                        );
+                                    }}
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params}
+                                            name="member"
+                                            label="From member"
+                                        />
+                                    )}
+                                />
+                                <Autocomplete
+                                    disablePortal
+                                    id="quotee-select"
+                                    className="search-input"
+                                    size="small"
+                                    options={quotees}
+                                    getOptionLabel={(option) => option.name}
+                                    onChange={(e, value) => {
+                                        console.log(value);
+                                        setFieldValue(
+                                            "quotee",
+                                            value !== null
+                                                ? value
+                                                : initialValues.quotee
+                                        );
+                                    }}
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params}
+                                            name="quotee"
+                                            label="Quotee"
+                                        />
+                                    )}
+                                />
+                                {/* </FormControl> */}
+                            </Stack>
+                            <Button variant="contained" type="submit">
+                                Search
+                            </Button>
+                        </Form>
+                    )}
+                </Formik>
+            </Container>
+        );
     }
-    
 }
 
 export default Search;
